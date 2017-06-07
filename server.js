1d725ff62972603b0f65d8c70553bb98d25109f4
lakshmisushma <lakshmisushma.578@gmail.com>
2017-06-07 10:58:19 +0000

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //sets the right header and status code
-  res.end('Hello Openshifttt Worldddddd\n');
+  res.end('Hello Openshifttt Worldddddd!!!!\n');
   //outputs string with line end symbol
 }).listen(8080);
 //sets port and IP address of the server
